d1df3148de9e1e2d20d90e208179e1ca56fd1eb6
Lyndell J. Rottmann <Lyndell@Lyndell.US>
2015-12-17 23:37:45 -0600

    seperate into two versions

--- getIP.php
+++ getIP.php
@@ -6,21 +6,6 @@
 
   echo $ip;
 
-  // longer, better way
-  $http_client_ip       = $_SERVER['HTTP_CLIENT_IP'];        // Internet ip address
-  $http_x_forwarded_for = $_SERVER['HTTP_X_FORWARDED_FOR'];  // checking for proxy server
-  $remote_addr          = $_SERVER['REMOTE_ADDR']; //
-   
-  if(!empty($http_client_ip)){
-    $ip = $http_client_ip;
-  }elseif(!empty($http_x_forwarded_for)){
-    $ip = $http_x_forwarded_for;
-  }else{
-  	$ip = $remote_addr;
-  }
-
-  echo $ip;
-
 
 
 
